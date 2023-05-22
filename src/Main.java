98cee1c02ea9ebba6fccabd5c5b23cfdb0aa69f5
Andyfer004 <99309349+Andyfer004@users.noreply.github.com>
2023-05-21 23:02:57 -0600

    Main predetereminado para el prgrama, aun no existen funciones del mismo.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("-----------------------------------------");
+        System.out.println("BIENVENIDO AL PROGRAMA DE RUTAS CIUDADES");
+        System.out.println("-----------------------------------------");
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+        do {
+            System.out.println("1. Ruta más corta entre dos ciudades");
+            System.out.println("2. Nombre ciudad centro del grafo");
+            System.out.println("3. Modificar grafo");
+            System.out.println("4. Salir");
+            System.out.print("Ingrese una opcion: ");
+            opcion = sc.nextInt();
+            switch (opcion) {
+                case 1:
+                    System.out.println("Ingresar ciudades");
+                    break;
+                case 2:
+                    System.out.println("Ingresar rutas");
+                    break;
+                case 3:
+                    System.out.println("Calcular rutas");
+                    break;
+                case 4:
+                    System.out.println("Salir");
+                    break;
+                default:
+                    System.out.println("Opcion no valida");
+                    break;
+            }
+        } while (opcion != 4);
+
     }
 }
