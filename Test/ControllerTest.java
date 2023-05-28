e2c7034666c89a235ab3a3af209097adf27d1b0c
Andyfer004 <99309349+Andyfer004@users.noreply.github.com>
2023-05-27 22:52:35 -0600

    Pruebas Junit de las clases

---
+++ Test/ControllerTest.java
@@ -0,0 +1,34 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ControllerTest {
+
+    @Test
+    public void leerArchivo() {
+    }
+
+    @Test
+    public void ruta_Corta() {
+    }
+
+    @Test
+    public void elegirClima() {
+    }
+
+    @Test
+    public void encontrarMatrizDistancias() {
+    }
+
+    @Test
+    public void encontrarMatrizRecorridos() {
+    }
+
+    @Test
+    public void calcularCentroGrafo() {
+    }
+
+    @Test
+    public void modificarGrafo() {
+    }
+}
---
+++ Test/FloydWarshallTest.java
@@ -0,0 +1,28 @@
+import junit.framework.TestCase;
+
+public class FloydWarshallTest extends TestCase {
+
+    public void testGetDistancias() {
+    }
+
+    public void testSetDistancias() {
+    }
+
+    public void testGetRecorridos() {
+    }
+
+    public void testSetRecorridos() {
+    }
+
+    public void testGetSIZE() {
+    }
+
+    public void testSetSIZE() {
+    }
+
+    public void testCalcularRutas() {
+    }
+
+    public void testGetVertices() {
+    }
+}
